79e0e602333689675debd179e0fe3fc3cf60de65
InsanitySorrow <e902469@gmail.com>
2015-05-19 17:51:37 -0500

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -6,7 +6,10 @@ exports.port = 8000;
 //   of strings. Each string should be either an IP address or a subnet given
 //   in CIDR notation. You should usually leave this as `false` unless you
 //   know what you are doing.
-exports.proxyip = false;
+exports.proxyip = ['127.0.0.0/8']; 
+
+exports.serverid = 'oblivion';
+exports.servertoken = 'fiSmBc2vAR0e';
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
